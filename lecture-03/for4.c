1286a4d2ccf3278962857a578ba0dd6334bed6e8
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-11 15:19:20 +0000

    Update and rename for4.c to lecture-03/for4.c

--- lecture-03/for4.c
+++ lecture-03/for4.c
@@ -3,8 +3,7 @@
   Written by: Joe Dorward
   Date: 06/01/00
 
-  Using a for() loop, this program asks for a start and stop value
-  then writes out the value, and character of the loop counter.
+  Using a for() loop, this program asks for a start and stop value then writes out the value, and character of the loop counter.
 */
 
 #include <stdio.h>
@@ -15,13 +14,11 @@ int loop_counter = 0,
     start_value = 0,
     stop_value = 0;
 
-
   printf("Please enter start value: ");
   scanf("%d",&start_value);
 
   printf("Please enter stop value: ");
   scanf("%d",&stop_value);
-
 
   for (loop_counter = start_value; loop_counter <= stop_value; loop_counter++)
   {
