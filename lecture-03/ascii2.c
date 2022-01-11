d4fcbe6a529e55f28d093d34cd3af62af128c6c3
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-11 15:30:36 +0000

    Update and rename ascii2.c to lecture-03/ascii2.c

--- lecture-03/ascii2.c
+++ lecture-03/ascii2.c
@@ -3,8 +3,7 @@
   Written by: Joe Dorward
   Date: 03/19/00
 
-  This program demonstrates the use of a for() loop,
-  by writing the lowercase letters in a list
+  This program demonstrates the use of a for() loop, by writing the lowercase letters in a list.
 */
 
 #include <stdio.h>
