842dd546c2a06485a13ac7cbefb1cf2bb31599d5
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-12 14:40:10 +0000

    Update and rename array1.c to lecture-04/array1.c

--- lecture-04/array1.c
+++ lecture-04/array1.c
@@ -4,10 +4,8 @@
   Date: 04/12/00
 
   This program declares an array of integers. 
-  The loop counter of a for() loop is used to assign values to 
-  the elements of the array.
-  Another for() loop prints the values of the elements to the 
-  screen.
+  The loop counter of a for() loop is used to assign values to the elements of the array.
+  Another for() loop prints the values of the elements to the screen.
 */
 
 #include <stdio.h>
@@ -18,18 +16,15 @@ int loop_counter,
     integer_array[25];
 
   /* assign value of loop counter into the array */
-
   for (loop_counter = 0; loop_counter < 25; loop_counter++)
   {
     integer_array[loop_counter] = loop_counter;
   }
 
   /* print the values of the array to the screen */
-
   for (loop_counter = 0; loop_counter < 25; loop_counter++)
   {
     printf("\n The value in element: %2d,",loop_counter);
     printf(" is: %d.",integer_array[loop_counter]);
   }
 }
-
