79d3f218792087f203f3bb0e81b5c9a029fe5cd8
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-13 13:10:43 +0000

    Update and rename class_list1.c to lesson-07/class_list1.c

--- lesson-07/class_list1.c
+++ lesson-07/class_list1.c
@@ -4,7 +4,7 @@
   Date: 04/26/00
 
   This program will open the file "class_list.dat",
-	and allow the user to add a name to the file
+  and allow the user to add a name to the file
 */
 
 #include <stdio.h>
@@ -17,23 +17,23 @@ FILE *pointer_to_file;   // the file pointer variable
 
   pointer_to_file = fopen("class_list.dat","a");   // open file for adding
 
-	printf("\nPlease enter the student\'s name: ");
-	gets(student_name);
+  printf("\nPlease enter the student\'s name: ");
+  gets(student_name);
 
   fprintf(pointer_to_file,"%s",student_name);
-	fputc('\n',pointer_to_file);   // start newline in the file
+  fputc('\n',pointer_to_file);   // start newline in the file
 
   fclose(pointer_to_file);   // close file
   // =========================================================================
   pointer_to_file = fopen("class_list.dat","r");   // open file for reading
 
-	fgets(student_name,25,pointer_to_file);   // read first name from file
+  fgets(student_name,25,pointer_to_file);   // read first name from file
 
   while (!feof(pointer_to_file))   // while not end-of-file
-	{
-		printf("%s",student_name);
-		fgets(student_name,25,pointer_to_file);   // read name from file
-	}
+  {
+    printf("%s",student_name);
+    fgets(student_name,25,pointer_to_file);   // read name from file
+  }
 
   fclose(pointer_to_file);   // close file
 }
