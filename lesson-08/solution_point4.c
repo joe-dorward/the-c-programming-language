e7bbbbb07c0700d5421e5d97ac298b40ffc75db7
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-13 13:40:08 +0000

    Update and rename point4.c to lesson-08/solution_point4.c

--- lesson-08/solution_point4.c
+++ lesson-08/solution_point4.c
@@ -4,7 +4,7 @@
   Date: 05/04/00
 
   This program uses an array of integers to store values, and uses
-	an array of pointers to keep track of where those values are stored
+  an array of pointers to keep track of where those values are stored
 */
 
 #include <stdio.h>
@@ -14,25 +14,25 @@ void main(void)
 int the_number = 0,       /* an integer variable */
     array_element,        /* for indicating the array element */
     number_array[10],     /* an integer array */
-		*pointer_array[10];   /* an array of pointers to integers */
+    *pointer_array[10];   /* an array of pointers to integers */
 
   /* ask for an integer value */
   printf(" Please enter an integer: ");  
 
-	/* put the value into the variable "the_number" */
-	scanf("%d",&the_number); 
+  /* put the value into the variable "the_number" */
+  scanf("%d",&the_number); 
   
   /* ask for the array element */
   printf(" Which array element should I put it in (0 - 9): ");  
 
   /* put the value into the variable "array_element" */
-	scanf("%d",&array_element);   
+  scanf("%d",&array_element);   
 
   /* put the value in "the_number" into the array element */
-	number_array[array_element] = the_number;   
+  number_array[array_element] = the_number;   
 
   /* set the first pointer in the pointer array, 
-	   to point to that element in the integer array */
+	to point to that element in the integer array */
   pointer_array[0] = &number_array[array_element];
 
 /* ======================================================================= */
@@ -43,7 +43,7 @@ int the_number = 0,       /* an integer variable */
 	printf(", is the value: %d",number_array[array_element]);
 
 	/* print to the screen, the value that the first pointer in the 
-	   pointer array points to */
+  pointer array points to */
   printf("\n The value pointed to, by the first pointer in the ");
 	printf("pointer array is: %d \n\n",*pointer_array[0]);
 
