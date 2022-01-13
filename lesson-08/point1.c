297bef880997662da20bac9f876b16b73b97c7c7
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-13 13:33:39 +0000

    Update and rename point1.c to lesson-08/point1.c

--- lesson-08/point1.c
+++ lesson-08/point1.c
@@ -4,7 +4,7 @@
   Date: 05/03/00
 
   This program prints the value of two integer variables,
-	and their address in memory
+  and their address in memory
 */
 
 #include <stdio.h>
@@ -14,13 +14,11 @@ void main(void)
 int first_number = 3,
     second_number = 7;
 
-
   /* print the vaues of the variables */
   printf("\n The first value, is: %d \n",first_number);
-	printf(" The second value, is: %d \n\n",second_number);
+  printf(" The second value, is: %d \n\n",second_number);
 
-
-	/* print the memeory addresses of the variables */
+  /* print the memeory addresses of the variables */
   printf("\n The first address, is: %d \n",&first_number);
-	printf(" The second address, is: %d \n\n",&second_number);
+  printf(" The second address, is: %d \n\n",&second_number);
 }
