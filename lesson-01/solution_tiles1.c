882a017bc815ae34de8394d1d1138dd9ea569206
Joe Dorward <94966952+joe-dorward@users.noreply.github.com>
2022-01-14 13:32:20 +0000

    Update and rename tiles1.c to solution_tiles1.c

--- lesson-01/solution_tiles1.c
+++ lesson-01/solution_tiles1.c
@@ -1,9 +1,9 @@
 /*
-	Program "tiles1.c"
-	Written by: Joe Dorward
-	Date: 05/17/00
+  Program "tiles1.c"
+  Written by: Joe Dorward
+  Date: 05/17/00
 
-	This program asks for the size of a square tile in inches.
+  This program asks for the size of a square tile in inches.
   Then the length and width of a room, then tells the user
   how many tiles are needed to tile the floor of that room.
 */
@@ -16,7 +16,7 @@ float tile_size = 0,
       tile_area = 0,
       number_of_tiles = 0,
       room_width = 0,
-	    room_length = 0,
+      room_length = 0,
       room_area = 0;
 
   printf(" Please enter the size of the tile (in inches): ");
